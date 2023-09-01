f7a22b2112e751cfc9a4f33a2ee34b21b44dbfe3
saturn691 <williamhuynh2002@gmail.com>
2023-09-01 19:06:27 +0000

    Initialise compiler

---
+++ src/compiler.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <iostream>
+#include <unistd.h>
+
+#include "cli.h"
+
+void compile(std::ostream &w)
+{
+    w << ".text" << std::endl;
+    w << ".globl f" << std::endl;
+    w << std::endl;
+
+    w << "f:" << std::endl;
+    w << "addi  t0, zero, 0" << std::endl;
+    w << "addi  t0, t0,   5" << std::endl;
+    w << "add   a0, zero, t0" << std::endl;
+    w << "ret" << std::endl;
+}
+
+// TODO: uncomment the below if you're using Flex/Bison.
+// extern FILE *yyin;
+
+int main(int argc, char **argv)
+{
+    // Parse CLI arguments, to fetch the values of the source and output files.
+    std::string sourcePath = "";
+    std::string outputPath = "";
+    if (parse_command_line_args(argc, argv, sourcePath, outputPath))
+    {
+        return 1;
+    }
+
+    // TODO: uncomment the below lines if you're using Flex/Bison.
+    // This configures Flex to look at sourcePath instead of
+    // reading from stdin.
+    // yyin = fopen(sourcePath, "r");
+    // if (yyin == NULL)
+    // {
+    //     perror("Could not open source file");
+    //     return 1;
+    // }
+
+    // Open the output file in truncation mode (to overwrite the contents)
+    std::ofstream output;
+    output.open(outputPath, std::ios::trunc);
+
+    // Compile the input
+    std::cout << "Compiling: " << sourcePath << std::endl;
+    compile(output);
+    std::cout << "Compiled to: " << outputPath << std::endl;
+
+    output.close();
+    return 0;
+}
