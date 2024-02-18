2fe9da5c6abf15e863cb078233ca62698f3c65ab
saturn691 <williamhuynh2002@gmail.com>
2024-02-18 19:53:33 +0000

    Add support for logical AND

--- compiler_tests/custom/007-float_logical_and_driver.c
+++ compiler_tests/custom/007-float_logical_and_driver.c
@@ -2,8 +2,8 @@ int f(float x, float y);
 
 int main()
 {
-    if (f(1.0, 0.0) != 1) return 1;
-    if (f(0.0, 1.0) != 1) return 1;
+    if (f(1.0, 0.0) != 0) return 1;
+    if (f(0.0, 1.0) != 0) return 1;
     if (f(1.0, 1.0) != 1) return 1;
     if (f(0.0, 0.0) != 0) return 1;
     return 0;
